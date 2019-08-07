a5ade715b10986971911f9403a893acef25be997
weixx <weixx3@139.com>
2019-08-08 07:54:15 +0800

    add springboot-mvc-mybatis

---
+++ springboot-mvc-mybatis/src/main/java/com/wxx/sbmm/SpringbootMvcMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.wxx.sbmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcMybatisApplication.class, args);
+	}
+
+}
